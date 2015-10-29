1e48a50c0f9e65d6c632b2bf4c30ba1ee54808d8
茗香墨语 <yaowen424@126.com>
2015-10-29 16:15:18 +0800

    添加了注释

--- app/src/main/java/com/yaowen/playvideotest/MainActivity.java
+++ app/src/main/java/com/yaowen/playvideotest/MainActivity.java
@@ -16,37 +16,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //初始化所有控件
         play= (Button) findViewById(R.id.play);
         pause= (Button) findViewById(R.id.pause);
         replay= (Button) findViewById(R.id.replay);
         videoView= (VideoView) findViewById(R.id.videoView);
 
+        //设置按钮的点击事件
         play.setOnClickListener(this);
         pause.setOnClickListener(this);
         replay.setOnClickListener(this);
-        initVideoPath();
+        initVideoPath();//初始视频路径
     }
 
     private void initVideoPath() {
         File file=new File(Environment.getExternalStorageDirectory(),"movie.3gp");
-        videoView.setVideoPath(file.getPath());
+        videoView.setVideoPath(file.getPath());//初始视频路径
     }
 
     @Override
     public void onClick(View v) {
         if (v==play){
             if (!videoView.isPlaying()){
-                videoView.start();
+                videoView.start();//开始播放
             }
         }
         if (v==pause){
             if (videoView.isPlaying()){
-                videoView.pause();
+                videoView.pause();//暂停播放
             }
         }
         if (v==replay){
             if (videoView.isPlaying()){
-                videoView.resume();
+                videoView.resume();//重新播放
             }
         }
 
