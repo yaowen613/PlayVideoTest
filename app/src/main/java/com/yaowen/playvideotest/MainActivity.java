6c5d6207028c0fd428632bdb3ed6a788f752f99e
茗香墨语 <yaowen424@126.com>
2015-10-29 16:10:48 +0800

    完成了代码

--- app/src/main/java/com/yaowen/playvideotest/MainActivity.java
+++ app/src/main/java/com/yaowen/playvideotest/MainActivity.java
@@ -1,13 +1,62 @@
 package com.yaowen.playvideotest;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button play,pause,replay;
+    private VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        play= (Button) findViewById(R.id.play);
+        pause= (Button) findViewById(R.id.pause);
+        replay= (Button) findViewById(R.id.replay);
+        videoView= (VideoView) findViewById(R.id.videoView);
+
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        replay.setOnClickListener(this);
+        initVideoPath();
+    }
+
+    private void initVideoPath() {
+        File file=new File(Environment.getExternalStorageDirectory(),"movie.3gp");
+        videoView.setVideoPath(file.getPath());
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==play){
+            if (!videoView.isPlaying()){
+                videoView.start();
+            }
+        }
+        if (v==pause){
+            if (videoView.isPlaying()){
+                videoView.pause();
+            }
+        }
+        if (v==replay){
+            if (videoView.isPlaying()){
+                videoView.resume();
+            }
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (videoView!=null){
+            videoView.suspend();
+        }
     }
 }
